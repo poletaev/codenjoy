6b8c927a17bb24d901a804f50449410f3e4cd60f
Andrey Poletaev <poletaev.andrew@gmail.com>
2019-12-07 22:14:23 +0600

    set AI3Solver as ai agent for coding-battle game

--- CodingDojo/games/coding-battle-2019/src/main/java/com/codenjoy/dojo/codingbattle2019/services/GameRunner.java
+++ CodingDojo/games/coding-battle-2019/src/main/java/com/codenjoy/dojo/codingbattle2019/services/GameRunner.java
@@ -25,7 +25,7 @@ package com.codenjoy.dojo.codingbattle2019.services;
 import com.codenjoy.dojo.client.ClientBoard;
 import com.codenjoy.dojo.client.Solver;
 import com.codenjoy.dojo.codingbattle2019.client.Board;
-import com.codenjoy.dojo.codingbattle2019.client.ai.AISolver;
+import com.codenjoy.dojo.codingbattle2019.client.ai.AI3Solver;
 import com.codenjoy.dojo.codingbattle2019.model.*;
 import com.codenjoy.dojo.services.AbstractGameType;
 import com.codenjoy.dojo.services.EventListener;
@@ -113,7 +113,7 @@ public class GameRunner extends AbstractGameType implements GameType {
 
     @Override
     public Class<? extends Solver> getAI() {
-        return AISolver.class;
+        return AI3Solver.class;
     }
 
     @Override
