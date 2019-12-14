fb0060dbb987d16052e680c2efc6c2a84b425941
Andrey Poletaev <poletaev.andrew@gmail.com>
2019-12-14 11:10:52 +0600

    add example solver

--- CodingDojo/games/codingbattle2019/src/main/java/com/codenjoy/dojo/codingbattle2019/client/ai/AI3Solver.java
+++ CodingDojo/games/codingbattle2019/src/main/java/com/codenjoy/dojo/codingbattle2019/client/ai/AI3Solver.java
@@ -298,7 +298,7 @@ public class AI3Solver implements Solver<Board> {
         WebSocketRunner.runClient(
                 // paste here board page url from browser after registration
                 //"http://codenjoy.com:80/codenjoy-contest/board/player/3edq63tw0bq4w4iem7nb?code=1234567890123456789",
-                "http://127.0.0.1:8080/codenjoy-contest/board/player/demo1@codenjoy.com?code=103734703798248113",
+                "http://192.168.1.150:8080/codenjoy-contest/board/player/demo1@codenjoy.com?code=103734703798248113",
                 new AI3Solver(new RandomDice()),
                 new Board());
     }
